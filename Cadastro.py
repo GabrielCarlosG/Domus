5aaac15c9b70da1dede7d05fd652915a60c65b44
Carlos Gabriel <gabrielcarlos331@gmail.com>
2025-05-16 23:01:49 -0300

    Uso de página padrão da Web, como modelo

---
+++ Cadastro.py
@@ -0,0 +1,6 @@
+nome = input('Qual o seu nome? ')
+email = input('Qual o seu e-mail? ')
+telefone = input('Qual o seu telefone? ')
+dataNasccimento = input('Informe a sua data de nascimento(dd/mm/aaaa): ')
+
+print(f'Seu nome é: {nome}, seu email: {email}, seu telefone: {telefone}, data de nascimento: {dataNasccimento}')
